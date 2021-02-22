2e4108eb84345f61c9911b18a50e45d9969c864d
Vinicius Skonicezny <vinicius.skonicezny@manipulae.com.br>
2021-02-22 11:40:53 -0300

    task/botao

--- src/components/atoms/Buttons/RegularButton/index.tsx
+++ src/components/atoms/Buttons/RegularButton/index.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
 
-const RegularButton = () => {
-    return (
-        <button>
+interface ButtonProps {
+    color: string,
+    children: string
+}
 
+const RegularButton = (props: ButtonProps) => {
+    const [ counter, setCounter] = React.useState(0)
+
+    const handleIncrement = () => {
+        setCounter(prevState => prevState + 1)
+    }
+
+    return (
+        <button  type="button" 
+            style={{backgroundColor: props.color}} 
+            onClick={handleIncrement}
+            value={counter} 
+            {...props}>
+            {props.children} <strong>{counter}</strong>
         </button>
     )
 }
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
+import { RegularButton } from "../../atoms/Buttons";
 
 
 const Home = () => {
     return (
+        <>
         <h1>
             Home
         </h1>
+        <div>
+            <RegularButton color={'#FF0058'} children={'Primeiro'} />
+            <RegularButton color={'#A1F008'} children={'Segundo'} />
+            <RegularButton color={'#21E0D8'} children={'Terceiro'} />
+        </div>
+        
+        </>
     )
 }
 
